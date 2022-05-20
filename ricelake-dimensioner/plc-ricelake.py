285bc7bbb3c32f242de29b50e89bc54cedb6099e
Kwesi Asare <15335005+koasare@users.noreply.github.com>
2022-05-20 09:54:32 -0400

    bit request works now. Test with Andrew

--- ricelake-dimensioner/plc-ricelake.py
+++ ricelake-dimensioner/plc-ricelake.py
@@ -9,7 +9,7 @@ dimensioner_endpoint = "https://localhost:5001"
 
 #package to send to Scanner Function
 def send_packet(barcode):
-       
+
     #create dictionary with Scanner Capture Information
     x = {
         "DimensionerName" : " ",
@@ -18,35 +18,31 @@ def send_packet(barcode):
         "UserField2": " ",
         "UserField3": " "
         }
-        
+
     #create json equivalent of dictionary
     y = json.dumps(x)
 
-    result = "POST /" + dimensioner_endpoint + "Capture" + y
+    result = "POST /" + dimensioner_endpoint + "/Capture" + y
 
     return result
+
 # Run Pyhton Code as long as PLC is running
 with PLC("192.168.1.100") as comm:
 
     #Package at Scanner Variable
-    package_present = comm.Read("SendToRestAPI")
+    
+    read_request = comm.Read("Highlight_Produced.Barcode[0]")
     information_sent = False
 
     while True:
+        package_present = comm.Read("SendToRestAPI")
 
+        if package_present.Value:
+            if information_sent == False:
+                api_post = send_packet(read_request.Value)
+                print(api_post)
+                information_sent = True
         
-#         #if package at scanner
-#         if package_present.Value:
-
-          #read barcode scanner information
-          read_request = comm.Read("Highlight_Produced.Barcode[0]")
-            
-          #create API string and store in variable
-          api_post = send_packet(read_request.Value)
-            
-          #Send API command to Scanner
-          print(api_post)
-            
-#         else:
-#             print(package_present.Value)
-#             continue
+        if information_sent:
+            if package_present.Value == False:
+                information_sent = False
